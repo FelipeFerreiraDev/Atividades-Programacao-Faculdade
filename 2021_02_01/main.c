d078b746c61c1a300d0dc231df0451d4e20e5d86
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-01-22 11:39:49 -0300

    first commit

---
+++ 2021_02_01/main.c
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char type[100][100];
+    
+    char board[100][100];
+    char boardOpc[100][100];
+    
+    int client[100];
+    
+    int payment[100];
+    
+    int count = 0, sair = 1;
+
+
+    /*
+        A)Processamento de um conjunto de locações, sendo que para cada locação são fornecidos
+            o tipo de veículo e sua respectiva placa;
+    */
+    while(sair==1){
+        printf("Digite o tipo do veículo: ");
+        scanf("%s", type[count]);
+        printf("Digite a placa do veículo: ");
+        scanf("%s", board[count]);
+        printf("Para continuar cadastrando mais uma locação digite 1, para ver as outras opções digite 0\n");
+        scanf("%d", &sair);
+        count++;
+    }
+    
+    count = 0;
+    
+    sair = 1;
+    
+    /*
+        B) Processamento de um conjunto de pagamentos de locação podendo ser armazenado a placa e
+            o código do cliente
+    */
+    
+    while(sair==1){
+        printf("Digite a placa do veículo para o pagamento: ");
+        scanf("%s", boardOpc[count]);
+        printf("Digite o código do cliente: ");
+        scanf("%d",&client[count]);
+        printf("Digite o valor que o cliente pagou em inteiros: ");
+        scanf("%d",&payment[count]);
+        printf("Para continuar cadastrando mais um pagamento digite 1, para ver as outras opções digite 0\n");
+        scanf("%d", &sair);
+        count++;
+    }
+    
+    printf("%d", client[1]);
+}
