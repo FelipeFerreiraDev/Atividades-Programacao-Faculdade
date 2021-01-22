6ed5078d9f7bc7391e76bdf0050c17e32453437c
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-01-22 11:43:04 -0300

    Arrumando Comentarios

--- 2021_02_01/main.c
+++ 2021_02_01/main.c
@@ -1,9 +1,8 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+ * Author: https://github.com/FelipeFerreiraDev
+ * 
+ * Link to the repository: https://github.com/FelipeFerreiraDev/Prog-Faculdade
+ * 
 *******************************************************************************/
 
 #include <stdio.h>
