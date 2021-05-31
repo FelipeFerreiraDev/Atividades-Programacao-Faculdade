07c34dfb6cb374d620c7a1ed16d68584a9fe7025
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-05-30 22:50:34 -0300

    first commit

--- JAVA/carro.java
+++ JAVA/carro.java
@@ -18,13 +18,15 @@ class Carro {
         System.out.printf("A marca do carro é: %s", marca);
         System.out.printf("\nSeu Modelo é: %s", modelo);
         System.out.printf("\nSeu ano de fabricação é: %d", ano);
-        System.out.printf("\nPossuindo %d portas\n", portas);
+        System.out.printf("\nPossuindo %d portas\n\n", portas);
     }   
 }
 
 class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
+        Scanner ler2 = new Scanner(System.in);
+        Scanner ler3 = new Scanner(System.in);
 
         Carro carro = new Carro();
         Carro carro2 = new Carro();
@@ -42,24 +44,24 @@ class Main {
         carro.Exibe();
         
         System.out.print("Digite o nome da marca do carro: ");
-        carro2.marca = ler.nextLine();
+        carro2.marca = ler2.nextLine();
         System.out.print("Digite o nome do modelo do carro: ");
-        carro2.modelo = ler.nextLine();
+        carro2.modelo = ler2.nextLine();
         System.out.print("Digite o ano do carro: ");
-        carro2.ano = ler.nextInt();
+        carro2.ano = ler2.nextInt();
         System.out.print("Digite a quantidade de portas do carro: ");
-        carro2.portas = ler.nextInt();
+        carro2.portas = ler2.nextInt();
         
         carro2.Exibe();
         
         System.out.print("Digite o nome da marca do carro: ");
-        carro3.marca = ler.nextLine();
+        carro3.marca = ler3.nextLine();
         System.out.print("Digite o nome do modelo do carro: ");
-        carro3.modelo = ler.nextLine();
+        carro3.modelo = ler3.nextLine();
         System.out.print("Digite o ano do carro: ");
-        carro3.ano = ler.nextInt();
+        carro3.ano = ler3.nextInt();
         System.out.print("Digite a quantidade de portas do carro: ");
-        carro3.portas = ler.nextInt();
+        carro3.portas = ler3.nextInt();
         
         carro3.Exibe();
    }
