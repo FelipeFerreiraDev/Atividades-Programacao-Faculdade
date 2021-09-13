65e4d29fc788e1c39ef7b9b4d38b276fd93f8dc4
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-09-13 09:38:16 -0300

    Lista Estrutura de dados

---
+++ JAVA/Estrutura de dados/Lista Avaliação 2/4- Lista Dinâmica Encadeada/Patrocinador.java
@@ -0,0 +1,11 @@
+public class Patrocinador {
+    String nomePatrocinador;
+
+    public Patrocinador(String nome){
+        nomePatrocinador = nome;
+    }
+
+    public String toString(){
+        return "Nome: "+nomePatrocinador;
+    }
+}
