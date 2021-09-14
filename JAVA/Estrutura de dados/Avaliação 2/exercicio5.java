5ea4efa8b57f9fba6eb68d2361cd62d50b610411
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-09-14 12:23:21 -0300

    exercicio 5

--- JAVA/Estrutura de dados/Avaliação 2/exercicio5.java
+++ JAVA/Estrutura de dados/Avaliação 2/exercicio5.java
@@ -1,24 +1,24 @@
-public void cadastra(No p) {
-    if(ultimo==0) {
-        lista[primeiro] = p;
+public void empilha(No n){
+        if(topo == -1 || topo<(this.TAM-1)){
+            topo = topo +1;
+            pilha[topo] = n;
+            System.out.println("Inserido na pilha "+n.tostring());
+        }
+        else{
+            System.out.println("PILHA CHEIA...");
+        }
     }
 
-    if(ultimo==0 || ultimo<(lista.length-1)) {
-        ultimo++;
-        lista[ultimo] = p;
+    public No desempilha(){
+        if(topo !=-1){
+            No temp = pilha[topo];
+            pilha[topo] = null;
+            topo = topo-1;
+            System.out.println("desempilha: "+temp.tostring());
+            return temp;
+        }
+        else{
+            System.out.println("Pilha vazia");
+            return null;
+        }
     }
-
-    anterior = ultimo-1;
-    proximo = ultimo+1;
-    
-    if(ultimo == 0 ) {
-        lista[ultimo].setAnterior(lista[anteriro]);
-    } else if(ultimo<15) {
-        lista[ultimo].setProximo(lista[proximo]);
-    }
-}
-
-public void remove() {
-    lista[ultimo] = null;
-    ultimo--;
-}
