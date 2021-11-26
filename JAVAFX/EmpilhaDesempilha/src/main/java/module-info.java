b7aebf8251ae154217db0bce7497d57cb7da006b
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-11-26 10:15:28 -0300

    update javafx

--- JAVAFX/EmpilhaDesempilha/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.empilhadesempilha {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.empilhadesempilha to javafx.fxml;
-    exports com.mycompany.empilhadesempilha;
-}
