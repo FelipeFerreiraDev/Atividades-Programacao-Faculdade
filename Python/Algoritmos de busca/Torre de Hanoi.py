218e05ac565848f02045f2ab6ca421157b656439
Felipe Henrique Camargos Ferreira <48157305+FelipeFerreiraDev@users.noreply.github.com>
2022-08-01 15:15:41 -0300

    Update Torre de Hanoi.py

--- Python/Algoritmos de busca/Torre de Hanoi.py
+++ Python/Algoritmos de busca/Torre de Hanoi.py
@@ -1,11 +1,10 @@
 def moveTower(height, fromPole, toPole, withPole):
     if height >= 1:
         moveTower(height-1,fromPole,withPole,toPole)
-        print("Movendo o disco: " , height)
-        moveDisk(fromPole,toPole)
+        moveDisk(height, fromPole,toPole)
         moveTower(height-1,withPole,toPole,fromPole)
 
-def moveDisk(fp,tp):
-    print("Movendo o disco de", fp, "para", tp, "\n")
+def moveDisk(height, fp, tp):
+    print(height, "->", fp,"->", tp, "\n")
 
-moveTower(3, "A", "C", "B")
+moveTower(10, "A", "C", "B")
